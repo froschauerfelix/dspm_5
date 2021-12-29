61cd21f723ded234498652a02fc32321fb22afb8
Felix Froschauer <felix.froschauer@student.uni-tuebingen.de>
2021-12-29 12:22:33 +0100

    Added country specific venues

--- Felix_Froschauer_AssignmentV.R
+++ Felix_Froschauer_AssignmentV.R
@@ -10,3 +10,86 @@ library(jsonlite)
 library(ggplot2)
 library(readr)
 
+# Interacting with the API - the basics
+
+# creating the data set with 20 rows
+size <- 20
+df_venues_de_page_1 <- 
+  data.frame(
+    name = character(size), 
+    city = character(size), 
+    postalcode = integer(size), 
+    address = character(size),
+    url = character(size), 
+    long = integer(size), 
+    lat = integer(size))
+
+
+# complete list of venues in Germany (Only page 1)
+venues_response <- GET(paste(
+  "https://app.ticketmaster.com/discovery/v2/venues.json?countryCode=DE&apikey", 
+  api_key, sep = "="))
+venues_list <- jsonlite::fromJSON(
+  content(venues_response, as = "text"))[["_embedded"]][["venues"]]
+
+# insert data from venues to data frame
+df_venues_de_page_1$name <- venues_list$name
+df_venues_de_page_1$city <- venues_list$city$name
+df_venues_de_page_1$postalcode <- venues_list$postalCode
+df_venues_de_page_1$address <- venues_list$address$line1
+df_venues_de_page_1$url <- venues_list$url 
+df_venues_de_page_1$long<- venues_list$location$longitude
+df_venues_de_page_1$lat<- venues_list$location$latitude
+
+
+# Interacting with the API - advanced
+
+# specify the country
+country <- "Germany"
+country_code <- "de"
+
+
+# file with data generating process
+df_venues <- read_csv(paste(paste("df_venues",country_code,sep="_"), "csv", sep="."))
+
+
+# correct type
+df_venues$long <- as.numeric(df_venues$long)
+df_venues$lat <- as.numeric(df_venues$lat)
+
+# eliminating extreme and missing values
+if (country == "Germany"){
+subset_venues <- subset(df_venues, lat > 47.271679)
+subset_venues <- subset(subset_venues, lat < 55.0846)
+
+subset_venues <- subset(subset_venues, long > 5.866943)
+subset_venues <- subset(subset_venues, as.numeric(long) < 15.043611)
+}
+
+
+if (country == "France") {
+  subset_venues <- subset(df_venues, lat > 40)
+  subset_venues <- subset(subset_venues_fr, lat < 51.05)
+  
+  subset_venues <- subset(subset_venues, long > -5.5)
+  subset_venues <- subset(subset_venues, as.numeric(long) < 10)
+  
+}
+
+title <- paste("Event locations across", country, sep=" ")
+
+
+# plot for specific country
+ggplot() +
+  geom_polygon(
+    aes(x = long, y = lat, group = group), data = map_data("world", region = country),
+    fill = "grey90",color = "black") +
+  theme_void() + coord_quickmap() +
+  labs(title = title,caption = "Source: ticketmaster.com") +
+  theme(title = element_text(size=8, face='bold'),
+        plot.caption = element_text(face = "italic")) +
+  geom_point(data = subset_venues, aes(x = long, y = lat), color = "red", size = 0.2) 
+
+
+
+
---
+++ get_all_data.R
@@ -0,0 +1,186 @@
+# data generating process
+
+
+
+# Germany
+df_venues_de <- 
+  data.frame(
+    name = character(), 
+    city = character(), 
+    postalcode = integer(), 
+    address = character(),
+    url = character(), 
+    long = integer(), 
+    lat = integer())
+
+# get page number
+
+
+for (m in 0:627) {
+  
+  # create temporary data frame
+  new_df <- 
+    data.frame(
+      name = character(20), 
+      city = character(20), 
+      postalcode = integer(20), 
+      address = character(20),
+      url = character(20), 
+      long = integer(20), 
+      lat = integer(20))
+  
+  # get page m of the API
+  venues_response_loop <- GET(paste(paste(paste("https://app.ticketmaster.com/discovery/v2/venues.json?countryCode=DE&apikey", api_key, sep = "="),"page", sep="&"),m, sep="="))
+  venues_list_loop <- jsonlite::fromJSON(content(venues_response_loop, as = "text"))[["_embedded"]][["venues"]]
+  
+  
+  # account for missing or incomplete values
+  new_df$name <- rep(NA, 20)
+  new_df$city <- rep(NA, 20)
+  new_df$postalcode <- rep(NA, 20)
+  new_df$address <- rep(NA, 20)
+  new_df$url <- rep(NA, 20)
+  new_df$long<-rep(NA, 20)
+  new_df$lat<- rep(NA, 20)
+  
+  # save the data in a new data frame
+  if (!is.null(venues_list_loop$name)) {
+    new_df$name <- venues_list_loop$name
+  }
+  
+  if (!is.null(venues_list_loop$city$name)) {
+    new_df$city <- venues_list_loop$city$name
+  }
+  
+  if (!is.null(venues_list_loop$postalCode)) {
+    new_df$postalcode <- venues_list_loop$postalCode
+  }
+  
+  if (!is.null(venues_list_loop$address$line1)) {
+    new_df$address <- venues_list_loop$address$line1
+  }
+  
+  if (!is.null(venues_list_loop$url)) {
+    new_df$url <- venues_list_loop$url
+  }
+  
+  
+  if (!is.null(venues_list_loop$location$longitude)) {
+    new_df$long <- venues_list_loop$location$longitude
+  }
+  
+  
+  if (!is.null(venues_list_loop$location$latitude)){
+    new_df$lat <- venues_list_loop$location$latitude
+  }
+  
+  
+  print(new_df)
+  # combine the iterating data frame and the permanent one
+  df_venues_de <- rbind(df_venues_de, new_df)
+  
+  print(m)
+  # sleep so that we do not recall too often
+  Sys.sleep(0.3)
+  
+  
+}
+
+
+# France
+
+
+# create final data set
+df_venues_fr <- 
+  data.frame(
+    name = character(), 
+    city = character(), 
+    postalcode = integer(), 
+    address = character(),
+    url = character(), 
+    long = integer(), 
+    lat = integer())
+
+# get page number
+
+for (m in 0:969) {
+  
+  # create temporary data frame
+  new_df <- 
+    data.frame(
+      name = character(20), 
+      city = character(20), 
+      postalcode = integer(20), 
+      address = character(20),
+      url = character(20), 
+      long = integer(20), 
+      lat = integer(20))
+  
+  # get page m of the API
+  venues_response_loop <- GET(paste(paste(paste("https://app.ticketmaster.com/discovery/v2/venues.json?countryCode=FR&apikey", api_key, sep = "="),"page", sep="&"),m, sep="="))
+  venues_list_loop <- jsonlite::fromJSON(content(venues_response_loop, as = "text"))[["_embedded"]][["venues"]]
+  
+  
+  # account for missing or incomplete values
+  new_df$name <- rep(NA, 20)
+  new_df$city <- rep(NA, 20)
+  new_df$postalcode <- rep(NA, 20)
+  new_df$address <- rep(NA, 20)
+  new_df$url <- rep(NA, 20)
+  new_df$long<-rep(NA, 20)
+  new_df$lat<- rep(NA, 20)
+  
+  # save the data in a new data frame
+  if (!is.null(venues_list_loop$name)) {
+    new_df$name <- venues_list_loop$name
+  }
+  
+  if (!is.null(venues_list_loop$city$name)) {
+    new_df$city <- venues_list_loop$city$name
+  }
+  
+  if (!is.null(venues_list_loop$postalCode)) {
+    new_df$postalcode <- venues_list_loop$postalCode
+  }
+  
+  if (!is.null(venues_list_loop$address$line1)) {
+    new_df$address <- venues_list_loop$address$line1
+  }
+  
+  if (!is.null(venues_list_loop$url)) {
+    new_df$url <- venues_list_loop$url
+  }
+  
+  
+  if (!is.null(venues_list_loop$location$longitude)) {
+    new_df$long <- venues_list_loop$location$longitude
+  }
+  
+  
+  if (!is.null(venues_list_loop$location$latitude)){
+    new_df$lat <- venues_list_loop$location$latitude
+  }
+  
+  
+  print(new_df)
+  # combine the iterating data frame and the permanent one
+  df_venues_fr <- rbind(df_venues_fr, new_df)
+  
+  print(m)
+  # sleep so that we do not recall too often
+  Sys.sleep(0.3)
+  
+  
+}
+
+
+# save the data sets in a csv file
+
+#write.csv(df_venues_de,"~/R/DS ProjectManagement/Assignment 5/dspm_5/df_venues_de.csv", row.names = TRUE)
+#write.csv(df_venues_fr,"~/R/DS ProjectManagement/Assignment 5/dspm_5/df_venues_fr.csv", row.names = TRUE)
+
+
+
+
+
+
