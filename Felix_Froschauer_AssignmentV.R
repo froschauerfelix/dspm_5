fd075b93ff4480f50330e0151f9dd5c15be0971e
Felix Froschauer <felix.froschauer@student.uni-tuebingen.de>
2021-12-29 10:28:34 +0100

    Added libaries and hidden variables

--- Felix_Froschauer_AssignmentV.R
+++ Felix_Froschauer_AssignmentV.R
@@ -1,2 +1,12 @@
 # Assignment 5 - DSPM
 
+# main addition
+
+
+# libraries and variables
+library(httr)
+source("~/R/DS ProjectManagement/Assignment 5/API_key.R")
+library(jsonlite)
+library(ggplot2)
+library(readr)
+
